14f0c1e8f9413804b47cfa239c109ae74b0f867a
kendrick807 <tkogt12@gmail.com>
2023-03-23 02:32:50 +0200

    increase max tokens of the answer.

--- server/src/chatController.js
+++ server/src/chatController.js
@@ -2,7 +2,7 @@ const methods = {};
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
 });
 let ans;
 const openai = new OpenAIApi(configuration);
@@ -13,8 +13,9 @@ methods.getAnswer = (q) => {
             openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: q,
+                max_tokens:2048,
             }).then((data)=>{
-                ans = data.data.choices[0].text;
+                console.log(data.data.choices[0].text)
                 resolve({status:200,message:data.data.choices[0].text});
 
             });
@@ -25,8 +26,6 @@ methods.getAnswer = (q) => {
     })
 }
 
-function getAns(){
-    return ans;
-}
+
 module.exports = methods;
 // module.exports = getAns();
