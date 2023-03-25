91e8f203e68586d9557cfa1f8599f517ef471b3c
kendrick807 <tkogt12@gmail.com>
2023-03-26 01:32:46 +0200

    removed node_modules folder

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -86,16 +86,9 @@ const Home = () => {
                     </button>
                 </form>
                 <div className="message-list-container" ref={containerRef}>
-                    {/*<ul className="message-list">*/}
-                    {/*    {questionArr.map(item => {*/}
-                    {/*        return <li key="{item}">{item}</li>;*/}
-                    {/*    })}*/}
-                    {/*</ul>*/}
+
                     <ul className="message-list" id="list">
 
-                        {/*{answerArr.map((item,i) => {*/}
-                        {/*    return <li key={i}>{item}</li>;*/}
-                        {/*})}*/}
                     </ul>
                 </div>
             </div>
@@ -106,64 +99,3 @@ const Home = () => {
 export default Home;
 
 
-
-// const Home = () => {
-//     const [question, setQuestion] = useState({});
-//     const array = [];
-//     const handleChange = (e) => {
-//         e.preventDefault();
-//
-//         setQuestion({
-//             question: e.target.value,
-//         });
-//     };
-//
-//     let customConfig = {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     };
-//
-//     const submitForm = (e) => {
-//         e.preventDefault();
-//
-//             axios
-//                 .post(`http://localhost:8000/api/getAnswer`, question,customConfig)
-//                 .then((res) => {
-//                     console.log(res);
-//                     console.log(res.data);
-//                     document.getElementById("testing").innerHTML = res.data.message;
-//                     array.push(res.data.message);
-//                     for(let i=0; i< array.length; i++){
-//                         console.log(array[i]);
-//                     }
-//                 });
-//
-//
-//         };
-//
-//     return (
-//         <div className="App">
-//             <form onSubmit={submitForm}>
-//                 <label>
-//                     User Name:
-//                     <input
-//                         type="text"
-//                         placeholder="Enter a message"
-//                         value={value}
-//                         onChange={handleChange}
-//                         className="message-input"
-//                         required
-//                         minLength={1}
-//                     />
-//                 </label>
-//                 <button type="submit" disabled={value < 1} className="send-message">
-//                     Send
-//                 </button>
-//             </form>
-//
-//         </div>
-//     );
-// }
-//
-// export default Home;
